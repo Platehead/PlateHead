51f4acee57f1ade6ea37e71363495b334ab9bfef
Lee, Kyuho <modulo641@gmail.com>
2017-11-04 15:59:13 +0900

    점프 예제 소스 추가

---
+++ Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class PlayerControll : MonoBehaviour {
+	public float inputThreshold;
+	public float jumpPower;
+	public float moveSpeed;
+	float distToGround;
+	Rigidbody2D rb;
+	int layerMask;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		var collider2D = GetComponent<Collider2D>();
+		distToGround = collider2D.bounds.extents.y;
+		Debug.Log(distToGround);
+		layerMask = 1 << LayerMask.NameToLayer("Ground");
+	}
+	void Update() {
+		var horizontalInput = Input.GetAxis("Horizontal");
+		if (Mathf.Abs(horizontalInput) > inputThreshold) {
+			Move(horizontalInput);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Jump();
+		}
+	}
+
+	void Jump() {
+		if (IsJumpable()) {
+			rb.AddForce(Vector2.up * jumpPower);
+		}
+		else
+			return;
+	}
+
+	bool IsJumpable() {
+		return Physics2D.Raycast(transform.position, 
+			Vector2.down, distToGround + 0.1f, layerMask);
+	}
+
+	void Move(float input) {
+		transform.position += Vector3.right * input * Time.deltaTime * moveSpeed;
+	}
+}
