e487a9e4c61abdd5ada532b8791b67442aca5a28
user-PC\user <jjs050626@naver.com>
2017-11-04 16:48:40 +0900

    UI작업중!

---
+++ Assets/move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public float speed;
+    public float JumpForce;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
