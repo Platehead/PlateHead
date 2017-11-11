e29339c5a256eb559a38d955936eaefb28aff0be
user-PC\user <jjs050626@naver.com>
2017-11-11 15:58:00 +0900

    ㅇㅇㄴㅇ

--- Assets/Scripts/Examples/CharacterExample.cs
+++ Assets/Scripts/Examples/CharacterExample.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterExample : MonoBehaviour {
-	IAttackable attack1;
+    IAttackable attack1;
 	IAttackable attack2;
 	HpManager hpManager;
 
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.Z))
+    private void Start()
+    {
+        attack1 = GetComponent<MeleeAtk>();
+    }
+
+    void Update() {
+		if (Input.GetKeyDown(KeyCode.F))
 			attack1.Attack();
 		
 		if (Input.GetKeyDown(KeyCode.X))
