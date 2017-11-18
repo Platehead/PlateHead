856c79fc33b919be77d64a7331c85c648a85ff13
DaveRipper <daveripper0020@gmail.com>
2017-11-18 16:46:21 +0900

    공격들이 마우스 향해 가도록 수정

---
+++ Assets/Scripts/Particle_AutoDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_AutoDestroy : MonoBehaviour {
+
+    ParticleSystem ps;
+
+	void Start ()
+    {
+        ps = GetComponent<ParticleSystem>();
+	}
+	
+	void Update ()
+    {
+		if (ps && !ps.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+	}
+}
