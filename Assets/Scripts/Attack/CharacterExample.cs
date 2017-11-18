bfd35b11889bfd2e1ca11686b7fe7cad3176c2c8
DaveRipper <daveripper0020@gmail.com>
2017-11-18 15:23:12 +0900

    미국 캐릭터 관련 변경

---
+++ Assets/Scripts/Attack/CharacterExample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterExample : MonoBehaviour
+{
+    IAttackable melee;
+    IAttackable ranged;
+    IAttackable skill_1;
+    IAttackable skill_2;
+    HpManager hpManager;
+
+    private void Start()
+    {
+        melee = GetComponent<MeleeAtk>();
+        ranged = GetComponent<Basic_Atk_US>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            melee.Attack();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            ranged.Attack();
+    }
+
+    void GetDamage(float damage)
+    {
+        hpManager.Hp -= damage;
+    }
+}
--- Assets/Scripts/Char_US/Atk_Manager_US.cs
+++ Assets/Scripts/Char_US/Atk_Manager_US.cs
@@ -2,25 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterExample : MonoBehaviour
+public class Atk_Manager_US : MonoBehaviour
 {
-    IAttackable attack1;
-    IAttackable attack2;
+    IAttackable melee;
+    IAttackable ranged;
+    IAttackable skill_1;
+    IAttackable skill_2;
     HpManager hpManager;
 
     private void Start()
     {
-        attack1 = GetComponent<MeleeAtk>();
-        attack2 = GetComponent<Basic_Atk_US>();
+        melee = GetComponent<MeleeAtk>();
+        ranged = GetComponent<Basic_Atk_US>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
-            attack1.Attack();
+            melee.Attack();
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
-            attack2.Attack();
+            ranged.Attack();
     }
 
     void GetDamage(float damage)
