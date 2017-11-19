c3ba10eca45d9662e7bec0d7c2312309ff86c21c
DaveRipper <daveripper0020@gmail.com>
2017-11-19 16:15:34 +0900

    미국캐릭터 관련

--- Assets/Scripts/Char_US/Skill_2_US.cs
+++ Assets/Scripts/Char_US/Skill_2_US.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class Skill_2_US : MonoBehaviour, IAttackable
 {
     public Transform Bomb;
-    public float Speed;
-    public float Time;
-    public ParticleSystem Eff;
 
 	public void Attack()
     {
@@ -18,10 +15,6 @@ public class Skill_2_US : MonoBehaviour, IAttackable
     {
         var bomb = Instantiate(Bomb);
         bomb.position = transform.position;
-        bomb.gameObject.AddComponent<Bomb_Move_US>();
-        bomb.gameObject.GetComponent<Bomb_Move_US>().Speed = Speed;
-        bomb.gameObject.GetComponent<Bomb_Move_US>().Eff = Eff;
-        bomb.gameObject.GetComponent<Bomb_Move_US>().time = Time;
 
         yield return null;
     }
